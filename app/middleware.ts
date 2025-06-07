751cdca40664a70b35f429a6f9db3cc4a96f5850
Taishi <for.prog.learning.th.com>
2025-06-08 01:17:24 +0900

    fix: middleware直かき

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,11 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { middleware as lineMiddleware } from '@line/bot-sdk';
+// import { middleware as lineMiddleware } from '@line/bot-sdk';
+import crypto from 'crypto';
 
 const lineConfig = {
 	channelAccessToken: process.env.NEXT_PUBLIC_LINE_CHANNEL_ACCESS_TOKEN || '',
-	channelSecret:
-		process.env.NEXT_PUBLIC_LINE_CHANNEL_SECRET ||
-		'492e9c5deca67bd09bdbe85c97b9488f',
+	channelSecret: process.env.NEXT_PUBLIC_LINE_CHANNEL_SECRET || '',
 };
 
 // // 署名検証を行う関数
@@ -19,19 +18,26 @@ const lineConfig = {
 // 	return computedSignature === signature;
 // }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function middleware(req: NextRequest) {
-	try {
-		// ミドルウェアによるシグネチャ検証
-		const response = lineMiddleware(lineConfig);
-		return response;
-	} catch (error) {
-		console.error('Error processing webhook:', error);
-		return NextResponse.json(
-			{ error: 'Authorization error at middleware' },
-			{ status: 500 }
-		);
+	// LINE Webhookの署名（X-Line-Signature）を取得
+	const signature = req.headers.get('X-Line-Signature') || '';
+
+	// リクエストボディを取得
+	const body = req.body ? JSON.stringify(req.body) : '';
+
+	// 署名の検証
+	if (!verifySignature(signature, body)) {
+		return new NextResponse('Unauthorized', { status: 401 });
 	}
+
+	return NextResponse.next();
+}
+
+function verifySignature(signature: string, body: string) {
+	const hmac = crypto.createHmac('SHA256', lineConfig.channelSecret);
+	hmac.update(body);
+	const computedSignature = hmac.digest('base64');
+	return computedSignature === signature;
 }
 
 export const config = {
